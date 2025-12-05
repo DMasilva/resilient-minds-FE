932232a935233f791312676767673c9af4744a67
DMasilva <davidonyango7872@gmail.com>
2025-12-05 15:03:52 -0600

    scroll to top

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -13,6 +14,7 @@ import Admin from './pages/Admin';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {/* Admin route - standalone without header/footer */}
         <Route path="/admin" element={<Admin />} />
